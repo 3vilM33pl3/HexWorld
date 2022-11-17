910b295a02ef04121f670af09b79d5c009dc56fa
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2022-11-17 11:39:18 +0000

    Add comments to the Pairing class

--- Source/HexWorldRuntime/Public/Utils/Pairing.h
+++ Source/HexWorldRuntime/Public/Utils/Pairing.h
@@ -5,25 +5,47 @@
 #include "CoreMinimal.h"
 #include "Pairing.generated.h"
 
+/**
+ * Class with pairing functions which maps two signed integers to a single, unique integer.
+ * It uses the Szudzik function to map two integers to a single integer. The Szudzik function
+ * can only map positive integers, so we first map the signed integers to unique unsigned integers
+ * and vice versa.
+ */
 UCLASS(BlueprintType)
 class HEXWORLDRUNTIME_API UPairing final: public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 public:
+	/**
+	 * Maps two signed integers to a single, unique integer.
+	 * @param X The first integer.
+	 * @param Y The second integer.
+	 * @return The unique integer.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Pairing", DisplayName="Signed Pairing")
 	static int64 Pair(int64 X, int64 Y);
 
+	/**
+	 * Maps a single, unique integer to two signed integers.
+	 * @param S The unique integer.
+	 * @param X The first integer.
+	 * @param Y The second integer.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Pairing", DisplayName="Signed UnPairing")
 	static void UnPair(int64 S, int64& X, int64& Y);
 	
 private:
+	// maps signed integer to a single, unique integer
 	static uint64 SignedTransform(int64 X);
 
+	// maps unsigned integer to a unique signed integer
 	static int64 UnsignedTransform(uint64 X);
-	
+
+	// pairs two unsigned integers to a single, unique unsigned integer
 	static uint64 SzudzikPairing(uint64 X, uint64 Y);
-	
+
+	// maps a single, unique unsigned integer to two unsigned integers
 	static void SzudzikUnPairing(uint64 S, int64& X, int64& Y);
 	
 };
