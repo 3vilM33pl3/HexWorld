19f479b8fda41905dd7da51bbde3ede12dc89dd7
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2022-11-17 11:47:38 +0000

    Add comments to HexWorld subsystem

--- Source/HexWorldRuntime/Public/UHexWorldSubsysten.h
+++ Source/HexWorldRuntime/Public/UHexWorldSubsysten.h
@@ -4,11 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "HexagonMap.h"
-#include "UObject/Object.h"
 #include "UHexWorldSubsysten.generated.h"
 
 /**
- * 
+ * Unreal Engine subsystem for HexagonMap. 
  */
 UCLASS()
 class HEXWORLDRUNTIME_API UUHexWorldSubsysten : public UGameInstanceSubsystem
@@ -16,10 +15,25 @@ class HEXWORLDRUNTIME_API UUHexWorldSubsysten : public UGameInstanceSubsystem
 	GENERATED_BODY()
 
 public:
+	/**
+	 * Destroys all Actors with tag "Hexagon".
+	 */
 	void DestroyAllHexagonActors();
+
+	/**
+	 * Initializes the subsystem. Creates a HexagonMap and retrieves the map data
+	 * from the backend server. 
+	 */
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	/**
+	 * Cleanup of the subsystem before it is removed.
+	 */
 	virtual void Deinitialize() override;
-	
+
+	/**
+	 * Returns the HexagonMap.
+	 */
 	UHexagonMap* GetHexagonMap() const { return HexagonMap; }
 
 private:
