2e4e173731d5f70fd176dd4f20a139b361879328
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2022-11-28 16:16:24 +0000

    Show next gate label in HUD

--- Source/HexWorldRuntime/Private/HUD/HexWorldHUD.cpp
+++ Source/HexWorldRuntime/Private/HUD/HexWorldHUD.cpp
@@ -4,6 +4,7 @@
 #include "HexWorldHUD.h"
 
 #include "CanvasItem.h"
+#include "Components/TextRenderComponent.h"
 #include "Engine/Canvas.h"
 #include "UObject/ConstructorHelpers.h"
 
@@ -24,18 +25,25 @@ void AHexWorldHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 	const FVector2D LogoDrawPosition( 0.0f, 0.0f);
 
+	// Draw logo top left
 	FCanvasTileItem TileItem( LogoDrawPosition, Logo3vilM33pl3->Resource, FLinearColor::White);
-    TileItem.BlendMode = SE_BLEND_Translucent;
+	TileItem.BlendMode = SE_BLEND_Translucent;
 	TileItem.Size = FVector2D( Logo3vilM33pl3->GetSizeX() / 4.0f , Logo3vilM33pl3->GetSizeY() / 4.0f );
-	TileItem.
-    Canvas->DrawItem( TileItem );
+	Canvas->DrawItem( TileItem );
 
+	// Draw narrowboat at bottom of screen
 	float Sizing = 2.0f;
 	const FVector2D NarrowboatDrawPosition( Canvas->ClipX * 0.5f - (NarrowboatTexture->GetSizeX() / 2) / Sizing, Canvas->ClipY - NarrowboatTexture->GetSizeY() / Sizing );
 	FCanvasTileItem NarrowboatTileItem( NarrowboatDrawPosition, NarrowboatTexture->Resource, FLinearColor::White);
 	NarrowboatTileItem.BlendMode = SE_BLEND_Translucent;
 	NarrowboatTileItem.Size = FVector2D( NarrowboatTexture->GetSizeX() / Sizing , NarrowboatTexture->GetSizeY() / Sizing );
-	Canvas->DrawItem( NarrowboatTileItem);     
+	Canvas->DrawItem( NarrowboatTileItem);
+
+	// Draw coordinates of next gate
+	if(LocationLabel) {
+		FString HUDString = LocationLabel->GetTextRender()->Text.ToString();
+        DrawText(HUDString, FColor::Yellow, Canvas->ClipX / 2 + (NarrowboatTexture->GetSizeX() / Sizing) / 2, Canvas->ClipY * 0.9f, nullptr);
+    }
 }
 
 void AHexWorldHUD::BeginPlay()
